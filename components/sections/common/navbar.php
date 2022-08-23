42f4cf6f708988af813ee8c5444d599e24a6b2f2
junedpothawale <junedwebsole@gmail.com>
2022-08-23 21:30:20 +0530

    Shalil Bug Removed

--- components/sections/common/navbar.php
+++ components/sections/common/navbar.php
@@ -27,7 +27,7 @@
   <!-- Topbar End -->
 
   <!-- Navbar Start -->
-  <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top px-4 px-lg-5">
+  <nav class="navbar navbar-expand-lg bg-primary navbar-light sticky-top px-4 px-lg-5">
     <a href="index.html" class="navbar-brand d-flex align-items-center">
       <h1 class="m-0">
         <img class="img-fluid me-3" src="img/logo.png" alt="" />
@@ -36,7 +36,7 @@
     <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarCollapse">
+    <div class="collapse navbar-collapse " id="navbarCollapse">
       <div class="navbar-nav mx-auto bg-light rounded pe-4 py-3 py-lg-0">
         <a href="index.html" class="nav-item nav-link active">Home</a>
         <a href="about.html" class="nav-item nav-link">About Us</a>
